d43f3969058b99e12223feb59ae7dc82777ad599
AwkwardBoy <xiaoyukeen@outlook.com>
2018-01-07 00:10:37 +0800

    Update

---
+++ chap1/ex1-5.cpp
@@ -0,0 +1,17 @@
+# include<iostream>
+
+int main(){
+	int v1=0; 
+	int v2=2;
+	std::cin >> v1;
+	std::cin >> v2;
+
+	std::cout << "The Sum of ";
+	std::cout << v1;
+	std::cout << " and ";
+	std::cout << v2;
+	std::cout << " is ";
+	std::cout << v1+v2 << std::endl;
+
+	return 0;
+}
